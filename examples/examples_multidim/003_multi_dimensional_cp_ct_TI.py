cea9ea16797818ec0fa9d40784c4c4421d34effe
misi9170 <michael.sinner@nrel.gov>
2025-09-15 16:49:10 -0600

    Update doc strings to remove Cp/Ct in favor of power/thrust_coefficient

--- examples/examples_multidim/003_multi_dimensional_cp_ct_TI.py
+++ examples/examples_multidim/003_multi_dimensional_cp_ct_TI.py
@@ -1,9 +1,9 @@
-"""Example: Multi-dimensional Cp/Ct with 2 Hs values
-This example follows the previous example but shows the effect of changing the Hs setting.
+"""Example: Multi-dimensional power/thrust coefficients with turbulence intensity
+This example follows the previous example, but demonstrating how a multidimensional turbine can be
+used to model the effect of turbulence intensity on power and thrust coefficient.
 
-NOTE: The multi-dimensional Cp/Ct data used in this example is fictional for the purposes of
-facilitating this example. The Cp/Ct values for the different wave conditions are scaled
-values of the original Cp/Ct data for the IEA 15MW turbine.
+NOTE: The multi-dimensional power/thrust coefficient data used in this example is fictional for the
+purposes of facilitating this example.
 """
 
 
@@ -16,20 +16,6 @@ from floris import FlorisModel, TimeSeries
 # Initialize FLORIS with the given input file.
 fmodel = FlorisModel("../inputs/gch_multi_dim_cp_ct_TI.yaml")
 
-# Make a second Floris instance with a different setting for Hs.
-# Note the multi-cp-ct file (iea_15MW_multi_dim_Tp_Hs.csv)
-# for the turbine model iea_15MW_floating_multi_dim_cp_ct.yaml
-# Defines Hs at 1 and 5.
-# The value in gch_multi_dim_cp_ct.yaml is 3.01 which will map
-# to 5 as the nearer value, so we set the other case to 1
-# for contrast.
-# fmodel_dict_mod = fmodel.core.as_dict()
-# fmodel_dict_mod["flow_field"]["multidim_conditions"]["TI"] = 0.10
-# # TODO: create a set_multidim_conditions method on fmodel (which reinitializes)
-# # Can check here if it is a valid key
-# # OR: add to set() method. Each value in multidim_conditions dict should be a scalar value
-# fmodel_hs_1 = FlorisModel(fmodel_dict_mod)
-
 # Set both cases to 3 turbine layout
 fmodel.set(layout_x=[0.0, 500.0, 1000.0], layout_y=[0.0, 0.0, 0.0])
 
@@ -40,6 +26,7 @@ time_series = TimeSeries(
 )
 fmodel.set(wind_data=time_series)
 
+# Loop over different turbulence intensities using set()
 fig, axarr = plt.subplots(1, 3, sharex=True, figsize=(12, 4))
 for ti, col in zip([0.06, 0.10], ["k", "r"]):
     fmodel.set(multidim_conditions={"TI": ti})

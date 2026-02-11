4cb14fc177abbc9a1d57a7a5632ee6d12f1cc4d5
misi9170 <michael.sinner@nrel.gov>
2026-02-11 15:55:06 -0700

    Add tests for FlowFieldPlanarGrid

--- tests/grid_unit_test.py
+++ tests/grid_unit_test.py
@@ -3,7 +3,7 @@ import logging
 import numpy as np
 import pytest
 
-from floris.core import TurbineGrid
+from floris.core import FlowFieldPlanarGrid, TurbineGrid
 
 
 def test_turbine_grid_init(caplog):
@@ -51,3 +51,52 @@ def test_turbine_grid_init(caplog):
         )
     assert "Non-positive z coordinates detected" in caplog.text
     caplog.clear()
+
+def test_flow_field_grid_init():
+
+    # Basic instantiation
+    FlowFieldPlanarGrid(
+        turbine_coordinates=np.array([[0.0, 0.0, 90.0]]),
+        turbine_diameters=np.array([126.0]),
+        wind_directions=np.array([270.0]),
+        normal_vector="x",
+        planar_coordinate=0.0,
+        grid_resolution=[2, 2],
+        x1_bounds=None,
+        x2_bounds=None,
+    )
+
+    # Invalid grid_resolution should raise TypeError
+    with pytest.raises(TypeError):
+        FlowFieldPlanarGrid(
+            turbine_coordinates=np.array([[0.0, 0.0, 90.0]]),
+            turbine_diameters=np.array([126.0]),
+            wind_directions=np.array([270.0]),
+            normal_vector="x",
+            planar_coordinate=0.0,
+            grid_resolution=2, # Invalid type (int instead of list)
+            x1_bounds=None,
+            x2_bounds=None,
+        )
+    with pytest.raises(TypeError):
+        FlowFieldPlanarGrid(
+            turbine_coordinates=np.array([[0.0, 0.0, 90.0]]),
+            turbine_diameters=np.array([126.0]),
+            wind_directions=np.array([270.0]),
+            normal_vector="x",
+            planar_coordinate=0.0,
+            grid_resolution=[2, 2, 3], # Invalid length (should be 2)
+            x1_bounds=None,
+            x2_bounds=None,
+        )
+    with pytest.raises(TypeError):
+        FlowFieldPlanarGrid(
+            turbine_coordinates=np.array([[0.0, 0.0, 90.0]]),
+            turbine_diameters=np.array([126.0]),
+            wind_directions=np.array([270.0]),
+            normal_vector="x",
+            planar_coordinate=0.0,
+            grid_resolution=[2.0, 2.0], # Invalid type in list (must be ints)
+            x1_bounds=None,
+            x2_bounds=None,
+        )
